e895877f6d06ba957c9c0c9d15cef4c0fe541e5e
Theprocess09 <tchakountefranklinarthur@gmail.com>
2026-06-29 14:29:37 +0100

    Creation de la classe Utilisateur

--- src/fondation/settings.py
+++ src/fondation/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,12 @@ WSGI_APPLICATION = 'fondation.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'africa_foundation',
+        'USER': 'africa_admin',
+        'PASSWORD': 'pentester09',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
@@ -115,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+AUTH_USER_MODEL = "users.Utilisateur"
---
+++ src/users/models.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Ici il s'agit de tout ce qui a un lien avec les utilisateurs
+"""
+Cette classe gère les utilsateurs de lapplication
+
+
+Le nom d'utilisateur : username 
+Le nom de l'individu : last_name
+Le prénom de l'individu : first_name
+Le mot de passe : password
+L'adresse mail : email
+La date de la première connexion : date_joined
+
+
+"""
+class Utilisateur(AbstractUser):
+    class Role(models.TextChoices):
+        CLIENT = 'client', 'CLIENT'
+        ADMIN = 'admin', 'ADMIN'
+
+    role = models.CharField(max_length=10, choices=Role.choices, default=Role.CLIENT)
+    email_verifie = models.BooleanField(default=False)
+    date_derniere_connexion = models.DateTimeField(blank=True, null=True)
